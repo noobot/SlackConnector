4bb9cf18cdd6800438e2a819c0ec2746d9cf20da
Simon Colmer <Simon.Colmer@totaljobsgroup.com>
2015-10-16 15:12:21 +0100

    Added some nuspec bits :)

--- src/SlackConnector/Properties/AssemblyInfo.cs
+++ src/SlackConnector/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SlackConnector")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A SlackApi wrapper...a shameless consumption of MargieBot's innards")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Workshop2")]
 [assembly: AssemblyProduct("SlackConnector")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
