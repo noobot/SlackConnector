ff531babc9187442743432ec8164756186255b2f
Simon Colmer <simon_colmer@hotmail.com>
2015-10-27 08:13:49 +0000

    Working towards mocking out handshacking

---
+++ src/SlackConnector/Models/Handshake/User.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace SlackConnector.Models.Handshake
+{
+    internal class User : Detail
+    {
+        [JsonProperty("real_name")]
+        public string RealName { get; set; }
+        [JsonProperty("is_bot")]
+        public bool IsBot { get; set; }
+        [JsonProperty("is_admin")]
+        public bool IsAdmin { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
---
+++ src/SlackConnector/Models/Handshake/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace SlackConnector.Models.Handshake
+{
+    internal class UserProfile
+    {
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+        [JsonProperty("real_name")]
+        public string RealName { get; set; }
+        [JsonProperty("real_name_normalized")]
+        public string RealNameNormalized { get; set; }
+        public string Email { get; set; }
+    }
+}
