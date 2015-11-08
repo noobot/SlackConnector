aaa704a55c3b9b11694e1aa35587908a8cc7d5ce
Simon C <simon_colmer@hotmail.com>
2015-11-08 23:06:20 +0000

    Added say test

---
+++ src/SlackConnector.Tests.Integration/SayTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using SlackConnector.Models;
+using SlackConnector.Tests.Integration.Configuration;
+
+namespace SlackConnector.Tests.Integration
+{
+    [TestFixture]
+    public class SayTests
+    {
+        [Test]
+        public void should_say_something_on_channel()
+        {
+            // given
+            var config = new ConfigReader().GetConfig();
+
+            ISlackConnector slackConnector = new SlackConnector();
+            slackConnector.Connect(config.Slack.ApiToken).Wait();
+
+            var message = new BotMessage
+            {
+                Text = "Test text for INT test",
+                ChatHub = slackConnector.ConnectedChannels.First(x => x.Name.Equals("#general", StringComparison.InvariantCultureIgnoreCase))
+            };
+
+            // when
+            slackConnector.Say(message).Wait();
+
+            // then
+
+        }
+    }
+}
