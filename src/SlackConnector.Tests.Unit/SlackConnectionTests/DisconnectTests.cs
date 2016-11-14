85b76fa46bb1aab555be773a33e62f7cc4eb6dfe
Simon C <simon_colmer@hotmail.com>
2016-11-14 19:08:43 +0000

    Added additional disconnect test

--- src/SlackConnector.Tests.Unit/SlackConnectionTests/DisconnectTests.cs
+++ src/SlackConnector.Tests.Unit/SlackConnectionTests/DisconnectTests.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using Moq;
 using NUnit.Framework;
-using Should;
 using SlackConnector.Connections.Sockets;
 using SlackConnector.Models;
 using SpecsFor;
@@ -42,6 +41,41 @@ namespace SlackConnector.Tests.Unit.SlackConnectionTests
             {
                 GetMockFor<IWebSocketClient>()
                     .Verify(x => x.Close(), Times.Once);
+            }
+        }
+
+        internal class given_connection_not_active : SpecsFor<SlackConnection>
+        {
+            private ConnectionInformation Info { get; set; }
+
+            protected override void Given()
+            {
+                Info = new ConnectionInformation
+                {
+                    Self = new ContactDetails { Id = "self-id" },
+                    Team = new ContactDetails { Id = "team-id" },
+                    Users = new Dictionary<string, SlackUser> { { "userid", new SlackUser() { Name = "userName" } } },
+                    SlackChatHubs = new Dictionary<string, SlackChatHub> { { "some-hub", new SlackChatHub() } },
+                    WebSocket = GetMockFor<IWebSocketClient>().Object
+                };
+
+                GetMockFor<IWebSocketClient>()
+                    .Setup(x => x.IsAlive)
+                    .Returns(false);
+
+                SUT.Initialise(Info);
+            }
+
+            protected override void When()
+            {
+                SUT.Disconnect();
+            }
+
+            [Test]
+            public void then_should_populate_self()
+            {
+                GetMockFor<IWebSocketClient>()
+                    .Verify(x => x.Close(), Times.Never);
             }
         }
     }
