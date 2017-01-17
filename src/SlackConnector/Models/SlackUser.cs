544043a5002ae5638da8563b3ef68ad5f87267ec
Matthew Swain <matthewswain@gmail.com>
2017-01-17 15:40:29 +0000

    Added email property to SlackUser.

--- src/SlackConnector/Models/SlackUser.cs
+++ src/SlackConnector/Models/SlackUser.cs
@@ -4,6 +4,7 @@
     {
         public string Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
 
         public string FormattedUserId
         {
