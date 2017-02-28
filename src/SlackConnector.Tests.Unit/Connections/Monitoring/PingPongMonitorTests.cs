2816a40d215e3fb4b954bdcce8259087ef592dcb
Simon Colmer <simon_colmer@hotmail.com>
2017-02-28 19:18:16 +0000

    Replace autofixture with attributes

--- src/SlackConnector.Tests.Unit/Connections/Monitoring/PingPongMonitorTests.cs
+++ src/SlackConnector.Tests.Unit/Connections/Monitoring/PingPongMonitorTests.cs
@@ -1,31 +1,19 @@
-﻿
-using System;
+﻿using System;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
-using Ploeh.AutoFixture;
-using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.NUnit3;
 using SlackConnector.Connections.Monitoring;
 
 namespace SlackConnector.Tests.Unit.Connections.Monitoring
 {
     [TestFixture]
-    public class PingPongMonitorTests
+    internal class PingPongMonitorTests
     {
-        private IFixture _fixture;
-
-        [SetUp]
-        public void Setup()
-        {
-            _fixture = new Fixture().Customize(new AutoMoqCustomization());
-            _fixture.Freeze<Mock<ITimer>>();
-        }
-
-        [Test]
-        public async Task should_call_ping_when_start_monitor_is_called()
+        [Test, AutoMoqData]
+        public async Task should_call_ping_when_start_monitor_is_called(PingPongMonitor monitor)
         {
             // given
-            var monitor = _fixture.Create<PingPongMonitor>();
             bool pingCalled = false;
             Func<Task> pingMethod = () => { pingCalled = true; return Task.CompletedTask; };
 
@@ -36,18 +24,16 @@ namespace SlackConnector.Tests.Unit.Connections.Monitoring
             Assert.That(pingCalled, Is.True);
         }
 
-        [Test]
-        public async Task should_start_timer_when_monitor_is_started()
+        [Test, AutoMoqData]
+        public async Task should_start_timer_when_monitor_is_started([Frozen]Mock<ITimer> timerMock, PingPongMonitor monitor)
         {
             // given
-            var monitor = _fixture.Create<PingPongMonitor>();
 
             // when
             await monitor.StartMonitor(() => Task.CompletedTask, () => Task.CompletedTask, TimeSpan.MinValue);
 
             // then
-            _fixture
-                .Create<Mock<ITimer>>()
+            timerMock
                 .Verify(x => x.RunEvery(It.IsAny<Action>(), TimeSpan.FromSeconds(5)), Times.Once);
         }
     }
