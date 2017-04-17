73e9a902a1372cfd2a3bb96ee4d2f24afc3df111
Simon Colmer <simon_colmer@hotmail.com>
2017-04-17 16:54:10 +0100

    Remove unused depedency from test

--- src/SlackConnector.Tests.Unit/SlackConnectionTests/PingTests.cs
+++ src/SlackConnector.Tests.Unit/SlackConnectionTests/PingTests.cs
@@ -3,8 +3,6 @@ using Moq;
 using NUnit.Framework;
 using Ploeh.AutoFixture.NUnit3;
 using SlackConnector.Connections;
-using SlackConnector.Connections.Clients.Channel;
-using SlackConnector.Connections.Models;
 using SlackConnector.Connections.Sockets;
 using SlackConnector.Connections.Sockets.Messages.Outbound;
 using SlackConnector.Models;
@@ -14,25 +12,15 @@ namespace SlackConnector.Tests.Unit.SlackConnectionTests
     internal class PingTests
     {
         [Test, AutoMoqData]
-        public async Task should_return_expected_slack_hub([Frozen]Mock<IConnectionFactory> connectionFactory,
-            Mock<IChannelClient> channelClient, Mock<IWebSocketClient> webSocket, SlackConnection slackConnection)
+        public async Task should_send_ping([Frozen]Mock<IConnectionFactory> connectionFactory, 
+            Mock<IWebSocketClient> webSocket, SlackConnection slackConnection)
         {
             // given
             const string slackKey = "key-yay";
-            const string userId = "some-id";
 
             var connectionInfo = new ConnectionInformation { WebSocket = webSocket.Object, SlackKey = slackKey };
             await slackConnection.Initialise(connectionInfo);
-
-            connectionFactory
-                .Setup(x => x.CreateChannelClient())
-                .Returns(channelClient.Object);
-
-            var returnChannel = new Channel { Id = "super-id", Name = "dm-channel" };
-            channelClient
-                .Setup(x => x.JoinDirectMessageChannel(slackKey, userId))
-                .ReturnsAsync(returnChannel);
-
+            
             // when
             await slackConnection.Ping();
 
