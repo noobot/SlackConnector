08f4bf58a2f627c9cde19dc33bfcb279c1504e77
Simon Colmer <simon_colmer@hotmail.com>
2017-04-17 14:26:54 +0100

    Remove SpecsFor from Pong test

--- src/SlackConnector.Tests.Unit/SlackConnectionTests/InboundMessageTests/PongTests.cs
+++ src/SlackConnector.Tests.Unit/SlackConnectionTests/InboundMessageTests/PongTests.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Moq;
 using NUnit.Framework;
+using Should;
+using SlackConnector.Connections.Models;
+using SlackConnector.Connections.Sockets;
 using SlackConnector.Connections.Sockets.Messages.Inbound;
+using SlackConnector.Models;
 
 namespace SlackConnector.Tests.Unit.SlackConnectionTests.InboundMessageTests
 {
+    internal class PongTests
+    {
+        [Test, AutoMoqData]
+        public async Task should_raise_event(Mock<IWebSocketClient> webSocket, SlackConnection slackConnection)
+        {
+            // given
+            var connectionInfo = new ConnectionInformation { WebSocket = webSocket.Object };
+            await slackConnection.Initialise(connectionInfo);
+            
+            DateTime lastTimestamp = DateTime.MinValue;
+            slackConnection.OnPong += timestamp =>
+            {
+                lastTimestamp = timestamp;
+                return Task.CompletedTask;
+            };
+
+            var inboundMessage = new PongMessage
+            {
+                Timestamp = DateTime.Now
+            };
+
+            // when
+            webSocket.Raise(x => x.OnMessage += null, null, inboundMessage);
+
+            // then
+            lastTimestamp.ShouldEqual(inboundMessage.Timestamp);
+        }
+    }
+
     internal class given_pong : BaseTest<PongMessage>
     {
         private DateTime _timestamp;
