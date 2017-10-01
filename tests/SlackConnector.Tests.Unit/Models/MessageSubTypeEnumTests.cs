c7c7c00c412f5590644eeafebd9ac19fe26bec58
Simon Colmer <simon@colmer.me>
2017-10-01 18:57:52 +0100

    Add tests around ordering of public enum values

--- tests/SlackConnector.Tests.Unit/Models/MessageSubTypeEnumTests.cs
+++ tests/SlackConnector.Tests.Unit/Models/MessageSubTypeEnumTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using Shouldly;
 using SlackConnector.Connections.Sockets.Messages.Inbound;
@@ -13,25 +15,58 @@ namespace SlackConnector.Tests.Unit.Models
         public void should_have_same_number_of_enum_values_as_internal_enum_type()
         {
             // given 
-            int numberOfInternalEnumValues = Enum.GetNames(typeof(MessageSubType)).Length;
-            int numberOfPublicEnumValues = Enum.GetNames(typeof(SlackMessageSubType)).Length;
+            int numberOfInternalEnumNames = Enum.GetNames(typeof(MessageSubType)).Length;
+            int numberOfPublicEnumNames = Enum.GetNames(typeof(SlackMessageSubType)).Length;
 
             // then
-            numberOfPublicEnumValues.ShouldBe(numberOfInternalEnumValues);
+            numberOfPublicEnumNames.ShouldBe(numberOfInternalEnumNames);
         }
 
         [Fact]
-        public void should_have_same_message_types_as_internal_model()
+        public void should_have_same_message_types_as_internal_models()
         {
             // given 
-            var internalEnumValues = Enum.GetNames(typeof(MessageSubType))
+            var internalEnumNames = Enum.GetNames(typeof(MessageSubType))
                 .Select(x => x.Replace("_", string.Empty))
                 .Select(x => x.ToLower());
-            var publicEnumValues = Enum.GetNames(typeof(SlackMessageSubType))
+            var publicEnumNames = Enum.GetNames(typeof(SlackMessageSubType))
                 .Select(x => x.ToLower());
 
             // then
-            publicEnumValues.All(x => internalEnumValues.Contains(x)).ShouldBeTrue();
+            publicEnumNames.All(x => internalEnumNames.Contains(x)).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_have_same_message_type_values_as_internal_models()
+        {
+            // given 
+            var internalEnumValues = GetEnumValues<MessageSubType>();
+            var publicEnumValues = GetEnumValues<SlackMessageSubType>();
+
+            // then
+            foreach (var publicEnumName in publicEnumValues.Keys)
+            {
+                publicEnumValues[publicEnumName].ShouldBe(internalEnumValues[publicEnumName]);
+            }
+        }
+
+        private static Dictionary<string, int> GetEnumValues<T>() where T : struct, IConvertible
+        {
+            var internalEnumNames = Enum.GetNames(typeof(T));
+
+            var vals = new Dictionary<string, int>();
+            foreach (var enumName in internalEnumNames)
+            {
+                T thingy;
+                Enum.TryParse(enumName, out thingy);
+
+                var name = enumName
+                    .Replace("_", string.Empty)
+                    .ToLower();
+
+                vals.Add(name, thingy.ToInt32(new NumberFormatInfo()));
+            }
+            return vals;
         }
     }
 }
