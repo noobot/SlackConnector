e0a55ef4490c33b7c50e6ef822c8958aff8ceeb6
Simon Colmer <simon@colmer.me>
2017-10-01 18:42:13 +0100

    Add public enum for messasge sub type

---
+++ src/SlackConnector/Models/SlackMessageSubType.cs
@@ -0,0 +1,30 @@
+﻿namespace SlackConnector.Models
+{
+    public enum SlackMessageSubType
+    {
+        Unknown = 0,
+        BotMessage,
+        MeMessage,
+        MessageChanged,
+        MessageDeleted,
+        ChannelJoin,
+        ChannelLeave,
+        ChannelTopic,
+        ChannelPurpose,
+        ChannelName,
+        ChannelArchive,
+        ChannelUnarchive,
+        GroupJoin,
+        GroupLeave,
+        GroupTopic,
+        GroupPurpose,
+        GroupName,
+        GroupArchive,
+        GroupUnarchive,
+        FileShare,
+        FileComment,
+        FileMention,
+        PinnedItem,
+        UnpinnedItem
+    }
+}
---
+++ tests/SlackConnector.Tests.Unit/Models/MessageSubTypeEnumTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Shouldly;
+using SlackConnector.Connections.Sockets.Messages.Inbound;
+using SlackConnector.Models;
+using Xunit;
+
+namespace SlackConnector.Tests.Unit.Models
+{
+    public class MessageSubTypeEnumTests
+    {
+        [Fact]
+        public void should_have_same_number_of_enum_values_as_internal_enum_type()
+        {
+            // given 
+            int numberOfInternalEnumValues = Enum.GetNames(typeof(MessageSubType)).Length;
+            int numberOfPublicEnumValues = Enum.GetNames(typeof(SlackMessageSubType)).Length;
+
+            // then
+            numberOfPublicEnumValues.ShouldBe(numberOfInternalEnumValues);
+        }
+
+        [Fact]
+        public void should_have_same_message_types_as_internal_model()
+        {
+            // given 
+            var internalEnumValues = Enum.GetNames(typeof(MessageSubType))
+                .Select(x => x.Replace("_", string.Empty))
+                .Select(x => x.ToLower());
+            var publicEnumValues = Enum.GetNames(typeof(SlackMessageSubType))
+                .Select(x => x.ToLower());
+
+            // then
+            publicEnumValues.All(x => internalEnumValues.Contains(x)).ShouldBeTrue();
+        }
+    }
+}
