f0333aa826bdc31fd28a78b4b714b44d9340b9f0
Simon Colmer <simon@colmer.me>
2017-12-28 22:16:25 +0000

    Make slack user immutable

--- src/SlackConnector/Models/SlackUser.cs
+++ src/SlackConnector/Models/SlackUser.cs
@@ -2,20 +2,20 @@
 {
     public class SlackUser
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Image { get; set; }
-        public string WhatIDo { get; set; }
-        public bool Deleted { get; set; }
-        public long TimeZoneOffset { get; set; }
-        public bool? Online { get; set; }
-        public bool IsBot { get; set; }
-        public bool IsGuest { get; set; }
-        public string StatusText { get; set; }
-        public bool IsAdmin { get; set; }
+        public string Id { get; internal set; }
+        public string Name { get; internal set; }
+        public string Email { get; internal set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public string Image { get; internal set; }
+        public string WhatIDo { get; internal set; }
+        public bool Deleted { get; internal set; }
+        public long TimeZoneOffset { get; internal set; }
+        public bool? Online { get; internal set; }
+        public bool IsBot { get; internal set; }
+        public bool IsGuest { get; internal set; }
+        public string StatusText { get; internal set; }
+        public bool IsAdmin { get; internal set; }
 
         public string FormattedUserId
         {
