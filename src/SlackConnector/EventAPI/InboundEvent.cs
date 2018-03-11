d71737c0d8ae94c29c1ed2a5ab5731cd1a066152
Yann <me.show@gmail.com>
2018-03-11 23:54:37 +0100

    Start Event API stuff

---
+++ src/SlackConnector/EventAPI/InboundEvent.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace SlackConnector.EventAPI
+{
+	public class InboundEvent
+	{
+		[JsonProperty("type")]
+		[JsonConverter(typeof(EventTypeConverter))]
+		public EventType Type { get; set; }
+
+		[JsonProperty("event_ts")]
+		public string EventTimestamp { get; set; }
+
+		[JsonProperty("user")]
+		public string User { get; set; }
+
+		[JsonProperty("ts")]
+		public string Timestamp { get; set; }
+
+		[JsonProperty("item")]
+		public string Item { get; set; }
+	}
+}
---
+++ src/SlackConnector/EventAPI/InboundOuterEvent.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace SlackConnector.EventAPI
+{
+	public class InboundOuterEvent
+	{
+		[JsonProperty("token")]
+		public string Token { get; set; }
+
+
+		[JsonProperty("team_id")]
+		public string TeamId { get; set; }
+
+		[JsonProperty("api_app_id")]
+		public string ApiAppId { get; set; }
+
+		[JsonProperty("event")]
+		public InboundEvent Event { get; set; }
+
+		[JsonProperty("authed_users")]
+		public string[] AuthedUsers { get; set; }
+
+		[JsonProperty("event_id")]
+		public string EventId { get; set; }
+
+		[JsonProperty("event_time")]
+		public int EventTime { get; set; }
+	}
+}
