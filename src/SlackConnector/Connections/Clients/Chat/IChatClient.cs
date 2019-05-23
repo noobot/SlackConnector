3cdf931aa12c9447ec53633ab03d492df541819a
Yann ROBIN <yb.robin@gmail.com>
2019-05-23 16:13:22 +0200

    fix

--- src/SlackConnector/Connections/Clients/Chat/IChatClient.cs
+++ src/SlackConnector/Connections/Clients/Chat/IChatClient.cs
@@ -11,9 +11,9 @@ namespace SlackConnector.Connections.Clients.Chat
 			IList<SlackAttachment> attachments, string threadTs = null, string iconUrl = null,
 			string userName = null, bool asUser = false, bool linkNames = true);
 
-		Task<MessageResponse> PostEphemeral(string slackKey, string channel, string user, string text, 
-			IList<SlackAttachment> attachments, string threadTs = null, string iconUrl = null, 
-			string userName = null, bool asUser = false, bool linkNames = true)
+		Task<MessageResponse> PostEphemeral(string slackKey, string channel, string user, string text,
+			IList<SlackAttachment> attachments, string threadTs = null, string iconUrl = null,
+			string userName = null, bool asUser = false, bool linkNames = true);
 
 		Task Update(string slackKey, string messageTs, string channel, string text,
 			IList<SlackAttachment> attachments, bool asUser = false, bool linkNames = true);
