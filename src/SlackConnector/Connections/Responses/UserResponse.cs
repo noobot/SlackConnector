c09e5d6460340c41dd10d48942903057cd8562fe
Yann ROBIN <yb.robin@gmail.com>
2019-06-10 16:22:52 +0200

    user response

--- src/SlackConnector/Connections/Responses/UserResponse.cs
+++ src/SlackConnector/Connections/Responses/UserResponse.cs
@@ -2,19 +2,19 @@
 
 namespace SlackConnector.Connections.Responses
 {
-	internal class IdentityResponse : StandardResponse
+	public class IdentityResponse : StandardResponse
 	{
 		public UserIdentity User { get; set; }
 
 		public TeamIdentity Team { get; set; }
 	}
 
-	internal class UserResponse : StandardResponse
+	public class UserResponse : StandardResponse
 	{
 		public User User { get; set; }
 	}
 
-	internal class UserCollectionResponse : CursoredResponse
+	public class UserCollectionResponse : CursoredResponse
     {
          public User[] Members { get; set; }
     }
