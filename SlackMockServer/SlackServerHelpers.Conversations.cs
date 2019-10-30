9a84d20673830d83f6b3da833de32838d71a3a5b
Yann ROBIN <yb.robin@gmail.com>
2019-10-30 15:23:44 +0100

    Add slack mock server project

---
+++ SlackMockServer/SlackServerHelpers.Conversations.cs
@@ -0,0 +1,68 @@
+﻿using SlackConnector.Connections.Clients.Conversation;
+using SlackConnector.Connections.Models;
+using SlackConnector.Connections.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WireMock;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+namespace SlackMockServer
+{
+    public static partial class SlackServerHelpers
+    {
+		public static SlackServer MockConversationList(this SlackServer server, params ConversationChannel[] conversations)
+		{
+			server.HttpServer.Given(Request.Create().WithPath(FlurlConversationClient.CONVERSATION_LIST_PATH))
+				.RespondWith(Response.Create().WithCallback(request =>
+				{
+					return new WireMock.ResponseMessage()
+					{
+						StatusCode = 200,
+						BodyData = new WireMock.Util.BodyData()
+						{
+							DetectedBodyType = WireMock.Util.BodyType.Json,
+							BodyAsJson = new ConversationCollectionReponse()
+							{
+								Ok = true,
+								Channels = conversations
+							}
+						}
+					};
+				}));
+
+			return server;
+		}
+
+		public static SlackServer MockDefaultConversationOpen(this SlackServer server)
+		{
+			string GetChannelId(RequestMessage request)
+			{
+				var users = string.Concat(request.GetParameterValuesFromPostOrGet("users"));
+
+				return $"DM{users}";
+			}
+
+			server.HttpServer.Given(Request.Create().WithPath(FlurlConversationClient.CONVERSATION_OPEN_PATH))
+				.RespondWith(Response.Create().WithCallback(request =>
+				{
+					return new WireMock.ResponseMessage()
+					{
+						StatusCode = 200,
+						BodyData = new WireMock.Util.BodyData()
+						{
+							DetectedBodyType = WireMock.Util.BodyType.Json,
+							BodyAsJson = new ConversationResponse()
+							{
+								Ok = true,
+								Channel = new ConversationChannel() { Id = GetChannelId(request) }
+							}
+						}
+					};
+				}));
+
+			return server;
+		}
+	}
+}
---
+++ SlackMockServer/SlackServerHelpers.cs
@@ -0,0 +1,86 @@
+﻿using SlackConnector.Connections.Clients.Chat;
+using SlackConnector.Connections.Clients.Conversation;
+using SlackConnector.Connections.Clients.Users;
+using SlackConnector.Connections.Models;
+using SlackConnector.Connections.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using WireMock;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+namespace SlackMockServer
+{
+	public static partial class SlackServerHelpers
+	{
+		public static string GenerateTimestamp()
+		{
+			var sTicks = DateTime.Now.Ticks.ToString();
+			var ticks = sTicks.Substring(sTicks.Length - 10, 10);
+			return ticks.Substring(0, 5) + "." + ticks.Substring(5, 5);
+		}
+
+		public static IEnumerable<string> GetParameterValuesFromPostOrGet(this RequestMessage request, string key)
+		{
+			IEnumerable<string> values = request.GetParameter(key);
+
+			if (values is null || !values.Any())
+			{
+				var qs = HttpUtility.ParseQueryString(request.Body.TrimStart('?'));
+				values = qs[key].Split(',');
+			}
+
+			return values;
+		}
+
+		public static string GetParameterValueFromPostOrGet(this RequestMessage request, string key)
+		{
+			IEnumerable<string> values = request.GetParameter(key);
+
+			if (values is null || !values.Any())
+			{
+				var qs = HttpUtility.ParseQueryString(request.Body.TrimStart('?'));
+				return qs[key];
+			}
+
+			return values?.FirstOrDefault();
+		}
+
+		private static string GetUserIdFromChannel(this RequestMessage request)
+		{
+			var channel = GetChannel(request);
+			if (channel.StartsWith("DM"))
+				return channel.Substring(2);
+			return null;
+		}
+
+		public static SlackServer MockUserInfo(this SlackServer server, User respondWithUser)
+		{
+
+			server.HttpServer.Given(Request.Create().WithPath(FlurlUserClient.USERS_INFO_PATH)
+				.WithParam("user", respondWithUser.Id))
+				.RespondWith(Response.Create().WithCallback(request =>
+				{
+					var response = new UserResponse()
+					{
+						User = respondWithUser,
+						Ok = true,
+					};
+					return new WireMock.ResponseMessage()
+					{
+						StatusCode = 200,
+						BodyData = new WireMock.Util.BodyData()
+						{
+							DetectedBodyType = WireMock.Util.BodyType.Json,
+							BodyAsJson = response
+						}
+					};
+				}));
+
+			return server;
+		}
+	}
+}
